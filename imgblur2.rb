e13da29b41d3072182db01d26eaabf29fe0e39b1
raquibbi <raquibbi@gmail.com>
2019-11-16 18:12:49 +0000

    added some imgblur 2 stuff

--- imgblur2.rb
+++ imgblur2.rb
@@ -1,7 +1,26 @@
 class Image
+
   def initialize(array) 
     @image = array
   end
+
+  def output_image
+    @image.each do |subarr|
+      str = ""
+      subarr.each do |item|
+        str << item.to_s 
+      end
+      puts str
+    end
+  end    
+
+#step 1: make copy of existing data 
+#step 2: loop over subarrays of old data by indices
+#step 3: loop over elements of subarrays by indices
+#step 4: check if our current element is a 1 
+#step 4b: update its neighbors with 1's in the COPY (flip with 4c)
+#step 4c: check if neighbor is contained in the boundaries
+#step 5: save updated copy over the old data 
 
   def blur
     #this copy is called a shallow copy
@@ -10,34 +29,26 @@ class Image
     copy = Marshal.load(Marshal.dump(@image))
     @image.each_index do |rowindex|
       @image[rowindex].each_index do |colindex|
-        if @image[rowindex][colindex] == 1 
+        if @image[rowindex][colindex] == 1
           @image[rowindex +1][colindex] = 1 #up
           @image[rowindex -1][colindex] = 1 #down
           @image[rowindex][colindex +1] = 1 #right
           @image[rowindex][colindex -1] = 1 #left
           #update neighbors with 1's 
+          #gonna be nested if statement
         end
       end
     end
     @image = copy 
     @image.each do |subarr|
       str = ""
-      subarr.each do |val|
-        str << val.to_s
+      subarr.each do |item|
+        str << intem.to_s
       end
       puts str
+    end
   end
 
-  # def output_image
-  #   @image.each do |subarr|
-  #     str = ""
-  #     subarr.each do |val|
-  #       str << val.to_s 
-  #     end
-  #     puts str
-  #   end
-  # end 
-    
 end
 
 image = Image.new([
@@ -47,7 +58,7 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
-# image.output_image
-# puts "\n"
+image.output_image
+puts "\n"
 image.blur
-end
+
